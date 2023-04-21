2856bb11f70fc0f8be18f83bcce5994ab627badf
aka334 <aka334@uky.edu>
2023-04-20 22:22:26 -0400

    Added batch_number to handle RTR functionality

--- subscriber.py
+++ subscriber.py
@@ -4,13 +4,19 @@ import sys
 import json
 import db
 import config
+
 username = 'team_10'
 password = 'myPassCS505'
 hostname = 'vbu231.cs.uky.edu'
 virtualhost = '10'
 
+
+# Initialize batch counters
+patient_batch_id = 0
+
+
 credentials = pika.PlainCredentials(username, password)
-parameters = pika.ConnectionParameters(hostname, 5672, virtualhost, credentials)
+parameters = pika.ConnectionParameters(hostname, 9099, virtualhost, credentials)
 
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
@@ -39,24 +45,33 @@ vax_list_queue = create_channel_binding('vax_list')
 print(' [*] Waiting for logs. To exit press CTRL+C')
 
 def callback_patient(ch, method, properties, body):
-    # print(" [x] %r:%r" % (method.routing_key, body))
+    global patient_batch_id
     data = json.loads(body)
+    for item in data:
+        item['batch_id'] = patient_batch_id
     print("Inserting patient")
     orientdb_client = db.connect_orientdb()
-    db.insert_patient_data(orientdb_client, data);
+    db.insert_patient_data(orientdb_client, data)
+    patient_batch_id += 1
 
-    
-    
 def callback_hospital(ch, method, properties, body):
+    global hospital_batch_id
     data = json.loads(body)
+    # for item in data:
+    #     item['batch_id'] = hospital_batch_id
     print("inserting hospital")
     db.insert_hospital_data(hospital_data_collection, data)
-   
+    # hospital_batch_id += 1
 
 def callback_vax(ch, method, properties, body):
+    global vax_batch_id
     data = json.loads(body)
-    print("inserting vax")
+    # for item in data:
+    #     item['batch_id'] = vax_batch_id
+    print("inserting vax: ", data)
     db.insert_vax_data(vax_data_collection, data)
+    # vax_batch_id += 1
+
     
 channel.basic_consume(
     queue=patient_list_queue, on_message_callback=callback_patient, auto_ack=True)
